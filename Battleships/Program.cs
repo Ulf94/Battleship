ef9bd22046436701a633b33218988e9a49d443da
Michal Szura <michal.szura@bytespack.com>
2023-06-13 14:26:28 +0200

    Code cleanup

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -5,19 +5,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
-services.AddScoped<ICustomRandom, CustomRandom>();
-services.AddScoped<IConsoleIO, ConsoleIO>();
-services.AddScoped<IPointsProvider, UserPointsProvider>();
-services.AddScoped<IPointsProvider, ComputerPointsProvider>();
-services.AddTransient<IPointsProvidersFactory, PointsProvidersFactory>();
+services.AddSingleton<ICustomRandom, CustomRandom>();
+services.AddSingleton<IConsoleIO, ConsoleIO>();
+services.AddSingleton<IPointsProvider, UserPointsProvider>();
+services.AddSingleton<IPointsProvider, ComputerPointsProvider>();
+services.AddSingleton<IPointsProvidersFactory, PointsProvidersFactory>();
 services.AddSingleton<IGame, Game>();
-services.AddScoped<IUserService, UserService>();
-services.AddScoped<IShipService, ShipService>();
+services.AddSingleton<IUserService, UserService>();
+services.AddSingleton<IShipService, ShipService>();
 
-var pointsProviderFactory = services.BuildServiceProvider().GetRequiredService<IPointsProvidersFactory>();
-var consoleService = services.BuildServiceProvider().GetRequiredService<IConsoleIO>();
 var game = services.BuildServiceProvider().GetRequiredService<IGame>();
 
 var gameRuntime = new GameRuntime(game);
-
-//var game = new Game(consoleService, OwnerType.COMPUTER, OwnerType.USER, pointsProviderFactory);
